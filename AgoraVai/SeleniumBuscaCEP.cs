a8bd6904122b0a7e96a51fc2360a5e3a7274d68f
Emanuel Santos Souza de Jesus <emanuel.jesus@barracred.dev.br>
2024-09-18 15:06:58 -0300

    Iniciando os testes com selenium

---
+++ AgoraVai/SeleniumBuscaCEP.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+
+namespace AgoraVai
+{
+    public class SeleniumBucasCEP: SeleniumBegin
+    {
+
+        [Test]
+        public void Test()
+        {
+            
+            driver.FindElement(By.Name("search_query")).SendKeys("Exterminador descolado");
+            driver.FindElement(By.Id("search-icon-legacy")).Submit();
+
+            Assert.That(driver.FindElement(By.XPath("//*[@id=\"video-title\"]/yt-formatted-string")).Text, Does.Contain("EXTERMINADOR DESCOLADO 🤪 | (Demon Slayer) | Prod. Sidney Scaccio | MHRAP"));
+        }
+    }
+}
